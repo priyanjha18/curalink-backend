d6aded48d7e998b01100834d5fb8e59a57d8186d
Priyan Jha <50802681+priyanjha18@users.noreply.github.com>
2025-11-18 05:04:51 +0530

    Set isProduction to true for session setup

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // --------------------------
 //  EXPRESS SESSION SETUP
 // --------------------------
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = true;
 
 app.use(
   session({
